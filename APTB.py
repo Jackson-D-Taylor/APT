93c9a0a19889643a48f12e4f5235134e7ee25e05
AstroJackson <jacksondtaylor4@gmail.com>
2026-09-08 13:48:34 -0400

    Revert "catching random things up"
    This reverts commit b04e0ad86dd41ffd3e3fd7a63dac6c997496155b.

--- APTB.py
+++ APTB.py
@@ -2082,7 +2082,7 @@ def main_for_loop(
 
         # update the model
         ########## m = f.model
-        print("Fitting again...")
+        print("Fitting again..."*1000)
         f.fit_toas(maxiter=4)
 
         if not save_state(
